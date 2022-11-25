959c69f6c79248ccd2c4ec8286b3c4d3ffdf3a37
peterjr123 <jsp020206@gmail.com>
2022-11-25 16:38:26 +0900

    feat: add ClientProxy class skeleton code

---
+++ src/network/ClientProxy.java
@@ -0,0 +1,13 @@
+package network;
+
+import data.DocumentList;
+
+public class ClientProxy {
+	public boolean login(String id, String password) {
+		return true;
+	}
+	
+	public DocumentList getDocumentList(int type) {
+		return null;
+	}
+}
