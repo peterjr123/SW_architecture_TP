6b92ebbcc2377b62a7f6469abc662066ce7167e8
peterjr123 <jsp020206@gmail.com>
2022-11-25 16:46:07 +0900

    feat: complete DocumentList class's basic functionalities

--- src/data/DocumentList.java
+++ src/data/DocumentList.java
@@ -1,5 +1,25 @@
 package data;
 
-public class DocumentList {
+import java.util.ArrayList;
 
+public class DocumentList {
+	private ArrayList<String> documentCourseName = new ArrayList<String>();
+	private ArrayList<String> documentName = new ArrayList<String>();
+	
+	public void addDocument(String course, String name) {
+		documentCourseName.add(course);
+		documentName.add(name);
+	}
+	
+	public int length() {
+		return documentCourseName.size();
+	}
+	
+	public String getCourse(int idx) {
+		return documentCourseName.get(idx);
+	}
+	
+	public String getDocumentName(int idx) {
+		return documentCourseName.get(idx);
+	}
 }
