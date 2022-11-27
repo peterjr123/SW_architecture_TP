7ea6b7edd5d2f6d54563e5c2bc7d27bb66f6a5b0
peterjr123 <jsp020206@gmail.com>
2022-11-27 13:26:37 +0900

    refactor: refactoring findMissingDocument method

--- src/client/GetDocumentManager.java
+++ src/client/GetDocumentManager.java
@@ -21,8 +21,7 @@ public class GetDocumentManager {
 	public void start() {
 		setDirectory("./테스트용/");
 		
-//		DocumentList ecampusDocumentList = getDocumentList();
-		DocumentList ecampusDocumentList = new DocumentList();
+		DocumentList ecampusDocumentList = getDocumentList();
 		
 		ecampusDocumentList.addDocument("소프트웨어 아키텍처", "강의자료1.pdf");
 		ecampusDocumentList.addDocument("소프트웨어 아키텍처", "강의자료2.pdf");
@@ -35,16 +34,94 @@ public class GetDocumentManager {
 		}
 	}
 	
+	
+	
 	private DocumentList findMissingDocument(DocumentList ecampusDocumentList, String documentType) {
-		Iterator courseDirectoryIterator = root.getSubDirectoryIterator();
-		HashMap<String, Directory> targetFileDirectory = new HashMap<String, Directory>();
+		// course별로 ecampus 파일들을 1차적으로 분류한다.
+		HashMap<String, DocumentList> classifiedDocumentList = classifyDocumentListByCourse(ecampusDocumentList);
+		
+		// 비교 대상 파일들이 담긴 디렉토리를 골라서 documentTypeDirectory에 마찬가지로 course별로 넣는다.
+		HashMap<String, Directory> classifiedDocumentTypeDirectory = classifyDocumentTypeDirectoryByCourse(root, documentType);
+		
+		// classifiedDocumentTypeDirectory와 classifiedDocumentList의 key값은 과목이름이다. -> key값을 비교하면 존재하지 않는 과목 디렉토리를 알아낼 수 있다.
+		// 존재하지 않는 과목 디렉토리를 생성한다.
+		for(String courseName : classifiedDocumentList.keySet()) {
+			if(!classifiedDocumentTypeDirectory.containsKey(courseName)) {
+				createCourseDirectoryTree(courseName, new String[] { documentType });
+			}
+		}
+		
+		// 위 정보를 바탕으로 존재하지 않는 문서 list를 작성한다.
+		DocumentList missingDocumentList = findMissingDocumentImpl(classifiedDocumentList, classifiedDocumentTypeDirectory);
+		
+		
+		return missingDocumentList;
+	}
+	
+	private DocumentList findMissingDocumentImpl(HashMap<String, DocumentList> classifiedDocumentList,
+			HashMap<String, Directory> classifiedDocumentTypeDirectory) {
+		
 		DocumentList missingDocumentList = new DocumentList();
 		
-		// course별로 파일들을 1차적으로 분류한다.
+		for(String courseName : classifiedDocumentList.keySet()) {
+			if(classifiedDocumentTypeDirectory.containsKey(courseName)) {
+				// 과목 디렉토리가 존재하는 경우에, 해당 과목 디렉토리를 탐색하여 대응되는 파일이 존재하는지 검사한뒤 존재하지 않으면 missingDocumentList에 추가한다.
+				DocumentList courseDocument = classifiedDocumentList.get(courseName);
+				for(int i = 0; i < courseDocument.length(); i++) {
+					String fileName = courseDocument.getDocumentName(i);
+					
+					if(!classifiedDocumentTypeDirectory.get(courseName).hasFile(fileName))
+						missingDocumentList.addDocument(courseName, fileName);
+				}
+			}
+			else {
+				// 과목 디렉토리가 존재하지 않는 경우는 전부다 missingDocumentList에 추가하기
+				DocumentList courseDocument = classifiedDocumentList.get(courseName);
+				for(int i = 0; i < courseDocument.length(); i++) {
+					missingDocumentList.addDocument(courseName, courseDocument.getDocumentName(i));
+				}
+			}
+		}
+		return missingDocumentList;
+	}
+
+	private void createCourseDirectoryTree(String courseName, String documentTypes[]) {
+		root.createSubDirectory(courseName);
+		
+		Iterator iterator = root.getSubDirectoryIterator();
+		while(iterator.hasNext()) {
+			Directory dir = (Directory) iterator.next();
+			if(dir.getName().equals(courseName)) {
+				for(String type : documentTypes) {
+					dir.createSubDirectory(type);
+				}
+			}
+		}
+	}
+
+	private HashMap<String, Directory> classifyDocumentTypeDirectoryByCourse(Directory root, String documentType) {
+		HashMap<String, Directory> documentTypeDirectory = new HashMap<String, Directory>();
+		
+		Iterator courseDirectoryIterator = root.getSubDirectoryIterator();
+		while(courseDirectoryIterator.hasNext()) {
+			Directory courseDirectory = (Directory) courseDirectoryIterator.next();
+			Iterator docTypeIterator = courseDirectory.getSubDirectoryIterator();
+			
+			while(docTypeIterator.hasNext()) {
+				Directory docTypeDirectory = (Directory) docTypeIterator.next();
+				
+				if(docTypeDirectory.getName().equals(documentType))
+					documentTypeDirectory.put(courseDirectory.getName(), docTypeDirectory);
+			}
+		}
+		return documentTypeDirectory;
+	}
+	
+	private HashMap<String, DocumentList> classifyDocumentListByCourse(DocumentList documentList) {
 		HashMap<String, DocumentList> classifiedList = new HashMap<String, DocumentList>();
-		for(int i = 0; i < ecampusDocumentList.length(); i++) {
-			String courseName = ecampusDocumentList.getCourse(i);
-			String docName = ecampusDocumentList.getDocumentName(i);
+		for(int i = 0; i < documentList.length(); i++) {
+			String courseName = documentList.getCourse(i);
+			String docName = documentList.getDocumentName(i);
 			if(classifiedList.containsKey(courseName)) {
 				classifiedList.get(courseName).addDocument(courseName, docName);
 			}
@@ -54,58 +131,7 @@ public class GetDocumentManager {
 				classifiedList.put(courseName, listByCourse);
 			}
 		}
-		
-		// 비교 대상 파일들이 담긴 디렉토리를 골라서 targetFileDirectory에 마찬가지로 course별로 넣는다.
-		while(courseDirectoryIterator.hasNext()) {
-			Directory courseDirectory = (Directory) courseDirectoryIterator.next();
-			Iterator docTypeIterator = courseDirectory.getSubDirectoryIterator();
-			while(docTypeIterator.hasNext()) {
-				Directory docTypeDirectory = (Directory) docTypeIterator.next();
-				
-				if(docTypeDirectory.getName().equals(documentType))
-					targetFileDirectory.put(courseDirectory.getName(), docTypeDirectory);
-			}
-		}
-		
-		// 모든 ecampus document에 대해서 targetFileDirectory안의 file 중 동일한 파일이 있는지 검사한다.
-		// 검사결과 동일한 파일이 존재하지 않으면 missingFileDocumentList에 추가한다.
-		for(String courseName : classifiedList.keySet()) {
-			if(targetFileDirectory.containsKey(courseName)) {
-				DocumentList courseDocument = classifiedList.get(courseName);
-				for(int i = 0; i < courseDocument.length(); i++) {
-					String fileName = courseDocument.getDocumentName(i);
-					Iterator files = targetFileDirectory.get(courseName).getSubFileIterator();
-					boolean hasFile = false;
-					while(files.hasNext()) {
-						if(((String)files.next()).equals(fileName)) {
-							hasFile = true;
-							break;
-						}
-					}
-					
-					if(!hasFile)
-						missingDocumentList.addDocument(courseName, fileName);
-				}
-			}
-			else {
-				// 디렉토리를 생성하고, missingDocumentList에 추가하기
-				root.createSubDirectory(courseName);
-				Iterator iterator = root.getSubDirectoryIterator();
-				while(iterator.hasNext()) {
-					Directory dir = (Directory) iterator.next();
-					if(dir.getName().equals(courseName)) {
-						dir.createSubDirectory(documentType);
-					}
-				}
-				
-				DocumentList courseDocument = classifiedList.get(courseName);
-				for(int i = 0; i < courseDocument.length(); i++) {
-					missingDocumentList.addDocument(courseName, courseDocument.getDocumentName(i));
-				}
-			}
-		}
-		
-		return missingDocumentList;
+		return classifiedList;
 	}
 
 	private boolean setDirectory(String rootDirectoryPath) {
@@ -118,7 +144,7 @@ public class GetDocumentManager {
 	}
 	
 	private DocumentList getDocumentList() {
-		return null;
+		return new DocumentList();
 	}
 	
 	private int getDocument() {
