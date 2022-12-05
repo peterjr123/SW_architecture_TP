302c22c309b5e5e54afb58fcac854a4cb5c9289d
peterjr123 <jsp020206@gmail.com>
2022-12-05 23:23:37 +0900

    feat: complete functionalities of downloadDocument

--- crawler/DocumentCrawler.py
+++ crawler/DocumentCrawler.py
@@ -6,7 +6,22 @@ import os
 import time
 
 class DocumentCrawler():
+    def downloadDocument(self, documentList):
+        filePathListPerSubject = {}
+        if(self.validAccount(self.id, self.pw) == False):
+            print("invalid account")
+            return
 
+        self.__login(self.id, self.pw)
+
+        subjectNames = self.__getSubjectNameList()
+
+        for subjectName in subjectNames:
+            self.__accessSubjectPage(subjectName)
+            filePathListPerSubject[subjectName] = self.downloadDocumentByCrawling(documentList)
+            self.driver.get('https://ecampus.konkuk.ac.kr/ilos/main/member/login_form.acl')
+        return filePathListPerSubject
+    
     def installChromeDriver(self):
         chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
         driver_path = f'./{chrome_ver}/chromedriver.exe'
@@ -19,9 +34,20 @@ class DocumentCrawler():
         return
 
     def validAccount(self, id, pw):
-        self.__login(id, pw)
+        # self.__login(id, pw)
         # print(f"id: {id}, pw={pw}")
         return True
+
+    def __getSubjectNameList(self):
+        subjectNames = []
+        subjects = self.driver.find_elements(By.CSS_SELECTOR, '.term_info ~ li')
+        for subject in subjects:
+            if(subject.get_attribute('class') != "term_info"):
+                rawName = subject.find_element(By.CLASS_NAME, 'sub_open').text
+                subjectNames.append(rawName[rawName.find(']')+1 : rawName.rfind('(')])
+            else: 
+                break
+        return subjectNames
 
     def getDocumentList(self):
         documentList = {}
@@ -32,31 +58,18 @@ class DocumentCrawler():
 
         self.__login(self.id, self.pw)
 
-        subjects = self.driver.find_elements(By.CSS_SELECTOR, '.term_info ~ li')
-        for subject in subjects:
-            if(subject.get_attribute('class') != "term_info"):
-                rawName = subject.find_element(By.CLASS_NAME, 'sub_open').text
-                subjectNames.append(rawName[rawName.find(']')+1 : rawName.rfind('(')])
-            else: 
-                break
+        subjectNames = self.__getSubjectNameList()
 
         for subjectName in subjectNames:
             self.__accessSubjectPage(subjectName)
             documentList[subjectName] = self.createDocumentListByCrawling()
             self.driver.get('https://ecampus.konkuk.ac.kr/ilos/main/member/login_form.acl')
         
-        for key in documentList:
-            print("강의 이름:", key)
-            if(documentList[key] is None):
-                continue
-            for item in documentList[key]:
-                print(item)
         return documentList
     
-    # def downloadDocument():
-    #     return
+    
 
-    def login(self, id, pw):
+    def __login(self, id, pw):
         self.driver.get('https://ecampus.konkuk.ac.kr/ilos/main/member/login_form.acl')
         self.driver.find_element(By.ID,'usr_id').send_keys(id)
         self.driver.find_element(By.ID,'usr_pwd').send_keys(pw)
@@ -74,18 +87,12 @@ class DocumentCrawler():
                 self.driver.execute_script('eclassRoom'+"('"+classNum+"')")
                 break
         return  
-    #
-    # @abstractmethod
-    # def createDocumentListByCrawling():
-    #     pass
+    
+    @abstractmethod
+    def createDocumentListByCrawling():
+        pass
 
+    @abstractmethod
+    def downloadDocumentByCrawling():
+        pass
 
-if __name__ == "__main__":
-    crawler = DocumentCrawler()
-    id = "qqazws7"
-    pwd = "lookat159~"
-    crawler.installChromeDriver()
-    while(True):
-        crawler.login(id, pwd)
-
-    #crawler.validAccount(id, pwd)
