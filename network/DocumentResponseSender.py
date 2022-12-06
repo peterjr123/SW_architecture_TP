3565a9a957c1ac7fa0379bbf64e7869936e41e0a
swkim0911 <kimsungwon364@gmail.com>
2022-12-06 15:37:12 +0900

    update network in server

--- network/DocumentResponseSender.py
+++ network/DocumentResponseSender.py
@@ -1,22 +1,20 @@
 class DocumentResponseSender:
 
-    def setsuccessloginresponse(self, client_socket):
+    def success_login_response(self, client_socket):
         response_msg = "HTTP/1.1 200 OK"
         client_socket.sendall(response_msg.encode(encoding="utf-8"))
 
 
-    def setGetDocumentListResponse(self, client_socket, responsebody):
+    def getDocumentList_response(self, client_socket, responsebody):
         response_msg = "HTTP/1.1 200 OK"+'\r\n'
-        response_msg +="Content-Length: "+str(len(responsebody))+"\r\n\r\n";
+        response_msg +="Content-Length: "+str(len(responsebody))+"\r\n\r\n"
         response_msg += responsebody
         client_socket.sendall(response_msg.encode(encoding="utf-8"))
 
-    def setsuccessDocumentresponse(sel, client_socket, responsebody):
-        response_msg = "HTTP/1.1 200 OK" + '\r\n'
-        response_msg += "Content-Length: " + str(len(responsebody)) + "\r\n\r\n";
-        response_msg += responsebody
+    def documentName_response(sel, client_socket, documentname):
+        response_msg = documentname + '\r\n'
         client_socket.sendall(response_msg.encode(encoding="utf-8"))
 
-    def setFailedResponse(self, client_socket):
+    def failed_response(self, client_socket):
         response_msg = "HTTP/1.1 400 Bad Request"
         client_socket.sendall(response_msg.encode(encoding="utf-8"))
